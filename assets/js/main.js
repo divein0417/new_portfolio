ded88aa3502047f54473af1ba87ed31d13dcbeb8
yoondahye <divein0417@gmail.com>
2024-09-17 04:25:49 +0900

    반응형 업데이트

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,12 @@
 $(function () {
-
+	
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty("--vh", vh + "px");
+	window.addEventListener("resize", function () {
+		let vh = window.innerHeight * 0.01;
+		document.documentElement.style.setProperty("--vh", vh + "px");
+	});
+	
 	// 비주얼 아이콘
 	gsap.to('.visual-sec .visual-icon img', {
 		yPercent: 10,
